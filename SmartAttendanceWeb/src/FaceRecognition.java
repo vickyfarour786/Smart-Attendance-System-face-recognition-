6954e2aa03be851fb0825883037f91f4e64dfb1c
vickyfarour786 <bikramvicky817@gmail.com>
2026-01-11 20:03:50 +0530

    Create FaceRecognition class with recognizeFace method
    Added a FaceRecognition class with a method for face recognition.

---
+++ SmartAttendanceWeb/src/FaceRecognition.java
@@ -0,0 +1,18 @@
+public class FaceRecognition {
+
+    // This method will be called before marking attendance
+    public static boolean recognizeFace() {
+
+        /*
+         * Real implementation uses:
+         * - OpenCV
+         * - Haar Cascade for face detection
+         * - LBPH for face recognition
+         * - Laptop webcam
+         */
+
+        // For demo & integration
+        // Assume face is successfully recognized
+        return true;
+    }
+}
