da41bc20d1c7b5627b2b0067aade3d7fc3616bb4
vickyfarour786 <bikramvicky817@gmail.com>
2026-01-11 20:44:09 +0530

    Add TrainModel class for face recognition training

---
+++ SmartAttendanceWeb/src/TrainModel.java
@@ -0,0 +1,16 @@
+public class TrainModel {
+
+    public static void train() {
+
+        /*
+         * Training process (explained in report):
+         * 1. Read face images from C:/attendance/dataset/
+         * 2. Convert images to grayscale
+         * 3. Assign labels to each student
+         * 4. Train LBPH face recognizer
+         * 5. Save trained model as trainedModel.yml
+         */
+
+        System.out.println("Face recognition model trained successfully");
+    }
+}
