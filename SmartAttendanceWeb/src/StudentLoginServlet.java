799326732160b3424fbf7518fddde74ac77d1fef
vickyfarour786 <bikramvicky817@gmail.com>
2026-01-12 20:06:26 +0530

    Implement file-based student login authentication

--- SmartAttendanceWeb/src/StudentLoginServlet.java
+++ SmartAttendanceWeb/src/StudentLoginServlet.java
@@ -1,4 +1,4 @@
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
@@ -8,14 +8,30 @@ public class StudentLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws IOException {
 
-        String uid = req.getParameter("uid");
-        String password = req.getParameter("password");
+        String inputUser = req.getParameter("uid");
+        String inputPass = req.getParameter("password");
 
-        // DEMO LOGIN (no storage)
-        if (uid.equals("101") && password.equals("student123")) {
+        BufferedReader br = new BufferedReader(
+            new FileReader("C:/attendance/students.txt"));
+
+        String line;
+        boolean found = false;
+        String uid = "";
+
+        while ((line = br.readLine()) != null) {
+            String[] data = line.split("\\|");
+            if (data[2].equals(inputUser) && data[3].equals(inputPass)) {
+                uid = data[0]; // UID
+                found = true;
+                break;
+            }
+        }
+        br.close();
+
+        if (found) {
             res.sendRedirect("studentDashboard.jsp?uid=" + uid);
         } else {
-            res.sendRedirect("studentLogin.jsp");
+            res.sendRedirect("studentlogin.jsp");
         }
     }
 }
